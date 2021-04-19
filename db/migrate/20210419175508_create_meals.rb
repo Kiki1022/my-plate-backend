6f92d143874f190d6f478fa1700f94804a142903
kinnari <kinnari1022@yahoo.com>
2021-04-19 13:57:27 -0400

    generate Meal model, controller and table

---
+++ db/migrate/20210419175508_create_meals.rb
@@ -0,0 +1,11 @@
+class CreateMeals < ActiveRecord::Migration[6.0]
+  def change
+    create_table :meals do |t|
+      t.string :img_src
+      t.string :location
+      t.text :caption
+
+      t.timestamps
+    end
+  end
+end
